5aeb2034d1ecfe880da1f64910253c8f119d07d4
Luana Leal <luana.leal.eq@gmail.com>
2020-09-03 01:37:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+  
+  attr_accessor :Knowledge, :name 
+
+    def new(name)
+      @name = name 
+    @Knowledge = []
+    end 
+    
+    def learn(string)
+    @Knowledge << string 
+    end 
 
 end
